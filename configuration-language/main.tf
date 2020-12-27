fabfb0cd615a94fb08c5b0df8d91dde0b6f257bb
Michael Russo <russo.mail@gmail.com>
2020-12-26 19:25:42 -0500

    Adds vpc and security group

--- configuration-language/main.tf
+++ configuration-language/main.tf
@@ -7,13 +7,52 @@ provider "random" {}
 
 resource "random_pet" "name" {}
 
-resource "aws_instance" "web" {
-  ami           = "ami-a0cfeed8"
-  instance_type = "t2.micro"
-  user_data     = file("init-script.sh")
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-a0cfeed8"
+  instance_type          = "t2.micro"
+  user_data              = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id              = module.vpc.public_subnets[0]
+  tags = {
     Name = random_pet.name.id
+  }
+}
+
+resource "aws_security_group" "web-sg" {
+  name   = "${random_pet.name.id}-sg"
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
